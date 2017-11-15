cd8c2ae3ef58df76f4523016fa3589241fd70fc9
Roberto <robertojimenezalonso@gmail.com>
2017-11-15 16:25:48 +0100

    Declaro las variables

--- app/src/main/java/com/example/roberto/actividad1/MainActivity.java
+++ app/src/main/java/com/example/roberto/actividad1/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.roberto.actividad1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnReturn, btnEdit, btnSave;
+    private EditText txtName, txtEmail, txtTlf, txtAddress;
+    private DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
